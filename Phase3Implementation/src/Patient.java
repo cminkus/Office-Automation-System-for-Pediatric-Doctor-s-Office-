d6c73ff9f9dc81904054d9afc357e785f31e3721
cminkus <114012366+cminkus@users.noreply.github.com>
2024-03-25 01:50:42 -0700

    Created updates to each file

---
+++ Phase3Implementation/src/Patient.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Patient extends User {
+    private List<String> medicalRecords;
+
+    public Patient(String username, String password) {
+        super(username, password, "Patient");
+        this.medicalRecords = new ArrayList<>();
+    }
+
+    @Override
+    public boolean login(String username, String password) {
+        return this.username.equals(username) && this.password.equals(password);
+    }
+
+    public void addMedicalRecord(String record) {
+        medicalRecords.add(record);
+    }
+
+    public List<String> getMedicalRecords() {
+        return medicalRecords;
+    }
+}

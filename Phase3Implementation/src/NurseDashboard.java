dc232cb2738456071c971fbca74415e0ee4d1fee
talhosni <143371390+talhosni@users.noreply.github.com>
2024-04-03 22:05:31 -0700

    Update NurseDashboard.java

--- Phase3Implementation/src/NurseDashboard.java
+++ Phase3Implementation/src/NurseDashboard.java
@@ -3,18 +3,28 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class NurseDashboard {
 
     public static void display(Stage window,  User user) {
         VBox layout = new VBox(10);
-        Label title = new Label("UPCOMING APPOINTMENTS");
+        Color backColor = Color.web("#EFD3FE");
+        layout.setStyle("-fx-background-color: #" + backColor.toString().substring(2, 8) + ";");
+        
+        Label title = new Label("TODAY’S APPOINTMENT");
+        title.setStyle("-fx-font-family: 'Times New Roman'; -fx-font-size: 18px;-fx-underline: true;");
+        title.setTranslateX(10);
+
         // Simulate patient list for today's appointments
         ListView<String> patientList = new ListView<>();
+        patientList.setMaxHeight(200);
+        patientList.setMaxWidth(300);
         patientList.getItems().addAll("Patient 1", "Patient 2", "Patient 3"); // Example data
 
         Button addPatientButton = new Button("Add New Patient");
+        addPatientButton.setTranslateX(20);
         addPatientButton.setOnAction(e -> {
             // Logic to add new patient
         	Stage addPatientStage = new Stage();
@@ -23,19 +33,22 @@ public class NurseDashboard {
         });
 
         // Example links to other pages
-        Button patientSearchButton = new Button("Patient Search");
-        Button vitalsButton = new Button("Vitals");
-        Button chatWithPatientButton = new Button("Contact Patients");
-        chatWithPatientButton.setOnAction(event -> {
-        	ChatScreen chat = new ChatScreen();
-        	chat.setRole(user.getRole());
-        	chat.start(window);
+
+        Button patientSearchButton = new Button("Patient Search and Contact");
+        patientSearchButton.setTranslateX(20);
+        patientSearchButton.setOnAction(event -> {
+        	PatientSearch patser = new PatientSearch();
+        	patser.start(window);
         });
+        Button vitalsButton = new Button("Vitals");
+        vitalsButton.setTranslateX(20);
 
         Button logoutButton = new Button("Logout");
+        logoutButton.setTranslateX(220);
+        logoutButton.setTranslateY(-10);
         logoutButton.setOnAction(e -> window.close()); // Simple logout logic
 
-        layout.getChildren().addAll(title, patientList, addPatientButton, patientSearchButton, vitalsButton, chatWithPatientButton, logoutButton);
+        layout.getChildren().addAll(title, patientList, addPatientButton, patientSearchButton, vitalsButton, logoutButton);
         Scene scene = new Scene(layout, 300, 400);
         window.setScene(scene);
         window.setTitle("Nurse Dashboard");

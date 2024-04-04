844c6e1b3a8fab608463893ec50c655c31abdbad
talhosni <143371390+talhosni@users.noreply.github.com>
2024-04-03 20:33:10 -0700

    Update NurseDashboard.java

--- Phase3Implementation/src/NurseDashboard.java
+++ Phase3Implementation/src/NurseDashboard.java
@@ -7,9 +7,9 @@ import javafx.stage.Stage;
 
 public class NurseDashboard {
 
-    public static void display(Stage window) {
+    public static void display(Stage window,  User user) {
         VBox layout = new VBox(10);
-        Label title = new Label("TODAY’S APPOINTMENT");
+        Label title = new Label("UPCOMING APPOINTMENTS");
         // Simulate patient list for today's appointments
         ListView<String> patientList = new ListView<>();
         patientList.getItems().addAll("Patient 1", "Patient 2", "Patient 3"); // Example data
@@ -17,17 +17,25 @@ public class NurseDashboard {
         Button addPatientButton = new Button("Add New Patient");
         addPatientButton.setOnAction(e -> {
             // Logic to add new patient
+        	Stage addPatientStage = new Stage();
+        	AddPatientInformation addPatientInformation = new AddPatientInformation();
+        	addPatientInformation.start(addPatientStage);
         });
 
         // Example links to other pages
         Button patientSearchButton = new Button("Patient Search");
         Button vitalsButton = new Button("Vitals");
-        Button contactPatientsButton = new Button("Contact Patients");
+        Button chatWithPatientButton = new Button("Contact Patients");
+        chatWithPatientButton.setOnAction(event -> {
+        	ChatScreen chat = new ChatScreen();
+        	chat.setRole(user.getRole());
+        	chat.start(window);
+        });
 
         Button logoutButton = new Button("Logout");
         logoutButton.setOnAction(e -> window.close()); // Simple logout logic
 
-        layout.getChildren().addAll(title, patientList, addPatientButton, patientSearchButton, vitalsButton, contactPatientsButton, logoutButton);
+        layout.getChildren().addAll(title, patientList, addPatientButton, patientSearchButton, vitalsButton, chatWithPatientButton, logoutButton);
         Scene scene = new Scene(layout, 300, 400);
         window.setScene(scene);
         window.setTitle("Nurse Dashboard");

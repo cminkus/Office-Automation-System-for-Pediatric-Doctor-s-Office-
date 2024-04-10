f4dcf76c72c59cdcfc9725fc6fd6af75d0f4bf48
talhosni <143371390+talhosni@users.noreply.github.com>
2024-04-10 12:17:59 -0700

    Update PatientDashboard.java

--- Phase3Implementation/src/PatientDashboard.java
+++ Phase3Implementation/src/PatientDashboard.java
@@ -1,6 +1,8 @@
 package Phase3Implementation;
 
 
+import java.io.File;
+
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,7 +11,7 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class PatientDashboard {
-    // Represents the dashboard interface for patients in the healthcare management system
+
     public static void display(Stage window, User user) {
         VBox layout = new VBox(10);
         
@@ -23,34 +25,9 @@ public class PatientDashboard {
         Label pID = new Label("Patient ID: " + user.getID());
         pID.setTranslateX(20);
         
-        String fileName = "src/Phase3Implementation/" + user.getID() + "_PatientFile.txt";
-        
-        readFile reader = new readFile();
-        reader.mainRead(fileName);
-
-        // Patient information is retrieved from the patient file
-        Label nameLabel = new Label("Name: " + reader.getFullName());
-        nameLabel.setTranslateX(20);
-        Label dobLabel = new Label("Date of Birth: " + reader.getDOB());
-        dobLabel.setTranslateX(20);
-        Label emailLabel = new Label("Email: " + reader.getEmail());
-        emailLabel.setTranslateX(20);
-        Label phoneLabel = new Label("Phone number: " + reader.getPhone());
-        phoneLabel.setTranslateX(20);
-        Label allergiesLabel = new Label("Allergies: " + reader.getAllergies());
-        allergiesLabel.setTranslateX(20);
-        Label pharmacyLabel = new Label("Pharmacy: " + reader.getPharmacy());
-        pharmacyLabel.setTranslateX(20);
-        Label insuranceLabel = new Label("Insurance: " + reader.getPatientInsurance());
-        insuranceLabel.setTranslateX(20);
-        Label HHLabel = new Label("Health History: " + reader.getPatientHH());
-        HHLabel.setTranslateX(20);
-        
-        
         Button editButton = new Button("Edit Phone Number or Email");
         editButton.setTranslateX(15);
         editButton.setOnAction(event -> {
-           // Allows patients to update their contact information
            ChangePatientInfo  change = new ChangePatientInfo();
            change.start(window, user);
         });
@@ -58,8 +35,7 @@ public class PatientDashboard {
         Button chatWithDoctorButton = new Button("Chat with Doctor/Nurse");
         chatWithDoctorButton.setTranslateX(15);
         chatWithDoctorButton.setOnAction(event -> {
-        	// Enables patients to communicate with healthcare providers
-            ChatScreen chat = new ChatScreen();
+        	ChatScreen chat = new ChatScreen();
         	chat.setRole(user.getRole());
         	chat.start(window, user);
         });
@@ -71,7 +47,54 @@ public class PatientDashboard {
             loginpage.start(window);
         });
         
-        layout.getChildren().addAll(title, pID,nameLabel, dobLabel,emailLabel,phoneLabel, allergiesLabel,pharmacyLabel,insuranceLabel,HHLabel, editButton, chatWithDoctorButton, logoutButton);
+        String fileName = "src/Phase3Implementation/" + user.getID() + "_PatientFile.txt";
+        File file = new File(fileName);
+        if(file.exists()) {
+	        readFile reader = new readFile();
+	        reader.mainRead(fileName);
+	                
+	        Label nameLabel = new Label("Name: " + reader.getFullName());
+	        nameLabel.setTranslateX(20);
+	        Label dobLabel = new Label("Date of Birth: " + reader.getDOB());
+	        dobLabel.setTranslateX(20);
+	        Label emailLabel = new Label("Email: " + reader.getEmail());
+	        emailLabel.setTranslateX(20);
+	        Label phoneLabel = new Label("Phone number: " + reader.getPhone());
+	        phoneLabel.setTranslateX(20);
+	        Label allergiesLabel = new Label("Allergies: " + reader.getAllergies());
+	        allergiesLabel.setTranslateX(20);
+	        Label pharmacyLabel = new Label("Pharmacy: " + reader.getPharmacy());
+	        pharmacyLabel.setTranslateX(20);
+	        Label insuranceLabel = new Label("Insurance: " + reader.getPatientInsurance());
+	        insuranceLabel.setTranslateX(20);
+	        Label HHLabel = new Label("Health History: " + reader.getPatientHH());
+	        HHLabel.setTranslateX(20);
+	        
+	        layout.getChildren().addAll(title, pID, nameLabel, dobLabel,emailLabel,phoneLabel, allergiesLabel,pharmacyLabel,insuranceLabel,HHLabel,editButton, chatWithDoctorButton, logoutButton);
+        
+        }else {
+        	Label nameLabel = new Label("Name: " );
+	        nameLabel.setTranslateX(20);
+	        Label dobLabel = new Label("Date of Birth: " );
+	        dobLabel.setTranslateX(20);
+	        Label emailLabel = new Label("Email: " );
+	        emailLabel.setTranslateX(20);
+	        Label phoneLabel = new Label("Phone number: " );
+	        phoneLabel.setTranslateX(20);
+	        Label allergiesLabel = new Label("Allergies: " );
+	        allergiesLabel.setTranslateX(20);
+	        Label pharmacyLabel = new Label("Pharmacy: " );
+	        pharmacyLabel.setTranslateX(20);
+	        Label insuranceLabel = new Label("Insurance: " );
+	        insuranceLabel.setTranslateX(20);
+	        Label HHLabel = new Label("Health History: " );
+	        HHLabel.setTranslateX(20);
+	        Utility.alert("No File found for patient", "Please contact a staff member to have a file created for you. ");
+	        layout.getChildren().addAll(title, pID, nameLabel, dobLabel,emailLabel,phoneLabel, allergiesLabel,pharmacyLabel,insuranceLabel,HHLabel,chatWithDoctorButton, logoutButton);
+        } 
+        
+        
+    
         Scene scene = new Scene(layout, 300, 400);
         window.setScene(scene);
         window.setWidth(300);
